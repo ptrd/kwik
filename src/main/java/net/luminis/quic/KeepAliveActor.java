7b2c5cc4b214c86c60fce5fe824bb40c29177c46
Peter Doornbosch <peter.doornbosch@luminis.eu>
2023-01-06 21:09:09 +0100

    fix: keep alive

--- src/main/java/net/luminis/quic/KeepAliveActor.java
+++ src/main/java/net/luminis/quic/KeepAliveActor.java
@@ -21,6 +21,7 @@ package net.luminis.quic;
 import net.luminis.quic.frame.PingFrame;
 import net.luminis.quic.send.Sender;
 
+import java.time.Clock;
 import java.time.Duration;
 import java.time.Instant;
 import java.util.concurrent.Executors;
@@ -33,22 +34,34 @@ import static net.luminis.quic.EncryptionLevel.App;
 
 public class KeepAliveActor {
 
+    private Clock clock;
     private final VersionHolder quicVersion;
     private final int keepAliveTime;
-    private final int peerIdleTimeout;
     private final Sender sender;
     private final Instant started;
-    private final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+    private final ScheduledExecutorService scheduler;
     private final int pingInterval;
     private volatile ScheduledFuture<?> scheduledTask;
 
+    /**
+     * @param quicVersion
+     * @param keepAliveTime       the time the connection should be kept alive in seconds
+     * @param peerIdleTimeout     the idle timeout of the peer, in milliseconds
+     * @param sender
+     */
     public KeepAliveActor(VersionHolder quicVersion, int keepAliveTime, int peerIdleTimeout, Sender sender) {
+        this(Clock.systemUTC(), quicVersion, keepAliveTime, peerIdleTimeout, sender, Executors.newScheduledThreadPool(1));
+    }
+
+    KeepAliveActor(Clock clock, VersionHolder quicVersion, int keepAliveTime, int peerIdleTimeout, Sender sender, ScheduledExecutorService scheduler) {
+        this.clock = clock;
         this.quicVersion = quicVersion;
         this.keepAliveTime = keepAliveTime;
-        this.peerIdleTimeout = peerIdleTimeout;
         this.sender = sender;
-        started = Instant.now();
-        pingInterval = peerIdleTimeout / 2;
+        this.scheduler = scheduler;
+
+        started = clock.instant();
+        pingInterval = peerIdleTimeout / 1000 / 2;
 
         scheduleNextPing();
     }
@@ -61,7 +74,7 @@ public class KeepAliveActor {
     }
 
     private void scheduleNextPing() {
-        Instant now = Instant.now();
+        Instant now = clock.instant();
         if (Duration.between(started, now).compareTo(Duration.of(keepAliveTime - pingInterval, SECONDS)) < 0) {
             scheduledTask = scheduler.schedule(() -> ping(), pingInterval, java.util.concurrent.TimeUnit.SECONDS);
         }
