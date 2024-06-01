06225be5bcf96021b39776a7c19a42f2bf0c8f9f
Peter Doornbosch <peter.doornbosch@luminis.eu>
2024-06-01 15:56:23 +0200

    prepare for multi module project

--- kwik.sh
+++ kwik.sh
@@ -2,7 +2,7 @@
 if [ ! -f libs/flupke.jar ]; then
   echo "No flupke jar found in libs directory; http requests will use HTTP 0.9!"
 fi
-kwikjar=`ls build/libs/kwik*.jar | grep -v javadoc | grep -v sources 2> /dev/null`
+kwikjar=`ls core/build/libs/kwik*.jar | grep -v javadoc | grep -v sources 2> /dev/null`
 if [ ! -f "$kwikjar" ]; then
     echo "Cannot find kwik jar file"
     exit
