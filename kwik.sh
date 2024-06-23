761e8e5da513c51ac08fc08d72f8c0bc588990c5
Peter Doornbosch <peter.doornbosch@luminis.eu>
2024-06-23 21:42:27 +0200

    fix: run script did not work for release version

--- kwik.sh
+++ kwik.sh
@@ -1,22 +1,22 @@
 #!/bin/sh
 
-if [ -f cli/build/distributions/kwik-cli-v*.zip ]; then
-    if [ -d dist/kwik-cli-v*/lib ]; then
-	if [ cli/build/distributions/kwik-cli-v*.zip -nt dist/kwik-cli-v*/lib ]; then
-	    rm -rf dist/kwik-cli-v*
+if [ -f cli/build/distributions/kwik-cli-*.zip ]; then
+    if [ -d dist/kwik-cli-*/lib ]; then
+	if [ cli/build/distributions/kwik-cli-*.zip -nt dist/kwik-cli-*/lib ]; then
+	    rm -rf dist/kwik-cli-*
 	fi
     fi
-    if [ ! -d dist/kwik-cli-v*/lib ]; then
+    if [ ! -d dist/kwik-cli-*/lib ]; then
 	mkdir -p dist
-	unzip -d dist cli/build/distributions/kwik-cli-v*.zip
-	touch dist/kwik-cli-v*/lib
+	unzip -d dist cli/build/distributions/kwik-cli-*.zip
+	touch dist/kwik-cli-*/lib
 	if [ ! -f libs/flupke.jar ]; then
 	    echo "No flupke jar found in libs directory; http requests will use HTTP 0.9!"
 	    echo
 	else
-	    sed -i '' 's@CLASSPATH=$APP_HOME@CLASSPATH=libs/flupke.jar:$APP_HOME@' dist/kwik-cli-v*/bin/kwik-cli
+	    sed -i '' 's@CLASSPATH=$APP_HOME@CLASSPATH=libs/flupke.jar:$APP_HOME@' dist/kwik-cli-*/bin/kwik-cli
 	fi
     fi
-    dist/kwik-cli-v*/bin/kwik-cli $*
+    dist/kwik-cli-*/bin/kwik-cli $*
     exit
 fi
