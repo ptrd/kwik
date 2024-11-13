6bf651c0656693679b43b969344401a22e01125f
Peter Doornbosch <peter.doornbosch@luminis.eu>
2024-11-13 21:52:17 +0100

    make script better handle situations when there is not 1 distribution file, but multiple or 0

--- kwik.sh
+++ kwik.sh
@@ -1,14 +1,28 @@
-#!/bin/sh
+#!/bin/bash
 
-if [ -f cli/build/distributions/kwik-cli-*.zip ]; then
+shopt -s nullglob
+distro=(cli/build/distributions/kwik-cli-*.zip)
+if [ ${#distro[@]} -lt 1 ]; then
+    echo "Error: no Kwik distro's found, execute 'gradle build' to get one."
+    exit
+fi
+if [ ${#distro[@]} -ne 1 ]; then
+    distro=`ls -t1 ${distro[@]} | head -1`
+    echo "Warning: multiple Kwik distro's found, using newest: $distro"
+    echo "if you agree...."
+    echo "so: using $distro"
+fi
+
+shopt -u nullglob
+if [ -f $distro ]; then
     if [ -d dist/kwik-cli-*/lib ]; then
-	if [ cli/build/distributions/kwik-cli-*.zip -nt dist/kwik-cli-*/lib ]; then
+	if [ $distro -nt dist/kwik-cli-*/lib ]; then
 	    rm -rf dist/kwik-cli-*
 	fi
     fi
     if [ ! -d dist/kwik-cli-*/lib ]; then
 	mkdir -p dist
-	unzip -d dist cli/build/distributions/kwik-cli-*.zip
+	unzip -q -d dist $distro
 	touch dist/kwik-cli-*/lib
 	if [ ! -f libs/flupke.jar ]; then
 	    echo "No flupke jar found in libs directory; http requests will use HTTP 0.9!"
@@ -17,6 +31,7 @@ if [ -f cli/build/distributions/kwik-cli-*.zip ]; then
 	    sed -i '' 's@CLASSPATH=$APP_HOME@CLASSPATH=libs/flupke.jar:$APP_HOME@' dist/kwik-cli-*/bin/kwik-cli
 	fi
     fi
+    echo 
     dist/kwik-cli-*/bin/kwik-cli $*
     exit
 fi
