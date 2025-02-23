aff283e3d8a17effdac04b0f9d5afd270da8f070
Peter Doornbosch <peter.doornbosch@luminis.eu>
2025-02-23 21:21:46 +0100

    fix: pass address

--- core/src/main/java/tech/kwik/core/impl/QuicClientConnectionImpl.java
+++ core/src/main/java/tech/kwik/core/impl/QuicClientConnectionImpl.java
@@ -498,7 +498,7 @@ public class QuicClientConnectionImpl extends QuicConnectionImpl implements Quic
                     log.raw("Start processing packet " + ++receivedPacketCounter + " (" + rawPacket.getLength() + " bytes)", rawPacket.getData(), 0, rawPacket.getLength());
                     log.debug("Processing delay for packet #" + receivedPacketCounter + ": " + processDelay.toMillis() + " ms");
 
-                    PacketMetaData metaData = new PacketMetaData(rawPacket.getTimeReceived(), null, receivedPacketCounter, rawPacket.getData().limit());
+                    PacketMetaData metaData = new PacketMetaData(rawPacket.getTimeReceived(), rawPacket.getPeerAddress(), receivedPacketCounter, rawPacket.getData().limit());
                     datagramProcessingChain.processDatagram(rawPacket.getData(), metaData);
 
                     sender.datagramProcessed(receiver.hasMore());
